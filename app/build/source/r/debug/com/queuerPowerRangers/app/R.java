94190376ffa8c66b278c0662c64348a450479065
Jose Rivas <jose_j_rivas@yahoo.com>
2014-01-20 13:26:09 -0500

    Trying to help

--- app/build/source/r/debug/com/queuerPowerRangers/app/R.java
+++ app/build/source/r/debug/com/queuerPowerRangers/app/R.java
@@ -867,7 +867,7 @@ containing a value of this type.
         public static final int undo_btn_bg_pressed=0x7f02005c;
     }
     public static final class id {
-        public static final int action_add_task=0x7f070049;
+        public static final int action_add_task=0x7f07004a;
         public static final int action_bar=0x7f07001c;
         public static final int action_bar_activity_content=0x7f070014;
         public static final int action_bar_container=0x7f07001b;
@@ -879,24 +879,24 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f070015;
         public static final int action_menu_presenter=0x7f070016;
         public static final int action_mode_close_button=0x7f070024;
-        public static final int action_settings=0x7f070048;
+        public static final int action_settings=0x7f070049;
         public static final int activity_chooser_view_content=0x7f070025;
         public static final int activity_feed=0x7f07003c;
         public static final int always=0x7f07000f;
         public static final int beginning=0x7f07000a;
-        public static final int btn_login=0x7f070040;
+        public static final int btn_login=0x7f070041;
         public static final int checkbox=0x7f07002d;
         public static final int collapseActionView=0x7f070011;
         public static final int container=0x7f07003d;
-        public static final int create_account=0x7f070042;
+        public static final int create_account=0x7f070043;
         public static final int default_activity_button=0x7f070028;
         public static final int dialog=0x7f070012;
         public static final int disableHome=0x7f070008;
         public static final int dropdown=0x7f070013;
         public static final int edit_query=0x7f070030;
         public static final int end=0x7f07000c;
-        public static final int et_password=0x7f07003f;
-        public static final int et_username=0x7f07003e;
+        public static final int et_password=0x7f070040;
+        public static final int et_username=0x7f07003f;
         public static final int expand_activities_button=0x7f070026;
         public static final int expanded_menu=0x7f07002c;
         public static final int home=0x7f070017;
@@ -904,17 +904,18 @@ containing a value of this type.
         public static final int icon=0x7f07002a;
         public static final int ifRoom=0x7f07000e;
         public static final int image=0x7f070027;
-        public static final int linear_project=0x7f070043;
+        public static final int linear_project=0x7f070044;
         public static final int listMode=0x7f070001;
         public static final int list_item=0x7f070029;
-        public static final int login_progress=0x7f070041;
+        public static final int login_progress=0x7f070042;
+        public static final int lv_tasks=0x7f07003e;
         public static final int middle=0x7f07000b;
         public static final int never=0x7f07000d;
         public static final int none=0x7f070009;
         public static final int normal=0x7f070000;
         public static final int progress_circular=0x7f070018;
         public static final int progress_horizontal=0x7f070019;
-        public static final int progress_login=0x7f070045;
+        public static final int progress_login=0x7f070046;
         public static final int radio=0x7f07002f;
         public static final int search_badge=0x7f070032;
         public static final int search_bar=0x7f070031;
@@ -933,11 +934,11 @@ containing a value of this type.
         public static final int split_action_bar=0x7f07001e;
         public static final int submit_area=0x7f070039;
         public static final int tabMode=0x7f070002;
-        public static final int text=0x7f070046;
+        public static final int text=0x7f070047;
         public static final int title=0x7f07002b;
         public static final int top_action_bar=0x7f070020;
-        public static final int tv_title=0x7f070044;
-        public static final int undo=0x7f070047;
+        public static final int tv_title=0x7f070045;
+        public static final int undo=0x7f070048;
         public static final int up=0x7f070021;
         public static final int useLogo=0x7f070003;
         public static final int withText=0x7f070010;
@@ -971,13 +972,14 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_feed=0x7f030017;
         public static final int activity_login=0x7f030018;
-        public static final int create_account=0x7f030019;
-        public static final int fragment_login=0x7f03001a;
-        public static final int list_project=0x7f03001b;
-        public static final int new_task=0x7f03001c;
-        public static final int progress_bar=0x7f03001d;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
-        public static final int undo_popup=0x7f03001f;
+        public static final int activity_project=0x7f030019;
+        public static final int create_account=0x7f03001a;
+        public static final int fragment_login=0x7f03001b;
+        public static final int list_project=0x7f03001c;
+        public static final int new_task=0x7f03001d;
+        public static final int progress_bar=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
+        public static final int undo_popup=0x7f030020;
     }
     public static final class menu {
         public static final int login=0x7f0c0000;
